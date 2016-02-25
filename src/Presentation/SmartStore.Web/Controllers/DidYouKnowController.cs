8612fb0ac2427e5be1a60a238da55025c1583cbb
James Bright <jb@rachelandjames.com>
2016-02-25 18:20:48 -0500

    Migrating work into QA

---
+++ src/Presentation/SmartStore.Web/Controllers/DidYouKnowController.cs
@@ -0,0 +1,60 @@
+﻿using Insight.Database;
+using SmartStore.Web.Framework.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartStore.Web.Controllers
+{
+    public class DidYouKnowController : PublicControllerBase
+    {
+        [ChildActionOnly()]
+        public ActionResult Random()
+        {
+            return View(GetRandomQuote());
+        }
+
+        private DidYouKnow GetRandomQuote()
+        {
+            var quotes = GetQuotes();
+            return quotes[RandomNumberGenerator.Next(0, quotes.Count)];
+        }
+
+        private List<DidYouKnow> GetQuotes()
+        {
+            if (_quotes == null)
+            {
+                lock (_lock)
+                {
+                    if (_quotes == null)
+                    {
+                        _quotes = ((new SqlConnection(ConfigurationManager.ConnectionStrings["EC"].ConnectionString))
+                            .As<DidYouKnowRepository>())
+                            .GetAll();
+                    }
+                }
+            }
+            return _quotes;
+        }
+        private static List<DidYouKnow> _quotes;
+        private static object _lock = new object();
+
+        private static Random RandomNumberGenerator = new Random();
+    }
+
+    public class DidYouKnow
+    {
+        int Id { get; set; }
+        public string Text { get; set; }
+    }
+
+    public interface DidYouKnowRepository
+    {
+        [Sql("SELECT * FROM QA_DidYouKnow")]
+        List<DidYouKnow> GetAll();
+    }
+}
---
+++ src/Presentation/SmartStore.Web/Controllers/ReferenceGameController.cs
@@ -0,0 +1,106 @@
+﻿using Insight.Database;
+using SmartStore.Services.Catalog;
+using SmartStore.Web.Framework.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartStore.Web.Controllers
+{
+    public class ReferenceGameController : PublicControllerBase
+    {
+        private readonly CatalogHelper _helper;
+        private readonly IProductService _productService;
+
+        #region Constructors
+        public ReferenceGameController(
+			IProductService productService,
+			CatalogHelper helper)
+        {
+			this._productService = productService;
+			this._helper = helper;
+        }
+        
+        #endregion
+
+        // GET: ReferenceGame
+        [OutputCache(Duration = 60 * 60, VaryByParam = "productId")]
+        public ActionResult Index(int productId)
+        {
+            ReferenceGameModel model = null;
+
+            var product = _productService.GetProductById(productId);
+            if (product == null)
+                return View(model);
+
+            var attributes = _helper.PrepareProductSpecificationModel(product);
+            if (attributes == null)
+                return View(model);
+
+            var attribute = attributes.ToList().Find(a => a.SpecificationAttributeName == "Related Game Title");
+            if (attribute == null)
+                return View(model);
+
+            var repo = ((new SqlConnection(ConfigurationManager.ConnectionStrings["EC"].ConnectionString)).As<ReferenceGameRepository>());
+            var game = repo.GetByName(attribute.SpecificationAttributeOption);
+            if (game == null)
+                return View(model);
+
+            model = new ReferenceGameModel()
+            {
+                Game = game,
+                Pictures = repo.GetGamePictures(game.Id),
+            };
+
+            return View(model);
+        }
+
+    }
+
+    public class ReferenceGameModel
+    {
+        public ReferenceGame Game { get; set; }
+        public List<ReferenceGamePicture> Pictures { get; set; }
+    }
+
+    public class ReferenceGame
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public int EntryId { get; set; }
+        public int Year { get; set; }
+        public string Manufacturer { get; set; }
+        public string AdditionalDescription { get; set; }
+        public string MonitorSync { get; set; }
+        public string MonitorComposite { get; set; }
+        public string MonitorOrientation { get; set; }
+        public string MonitorResolution { get; set; }
+        public string MonitorType { get; set; }
+        public string ConversionType { get; set; }
+        public string NumberPlayers { get; set; }
+    }
+
+    public class ReferenceGamePicture
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public string Description { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+    public interface ReferenceGameRepository
+    {
+        [Sql("SELECT * FROM QA_ReferenceGame WHERE Name=@name")]
+        ReferenceGame GetByName(string name);
+
+        [Sql("SELECT * FROM QA_ReferenceGamePicture WHERE ReferenceGameId=@id")]
+        List<ReferenceGamePicture> GetGamePictures(int id);
+    }
+}
